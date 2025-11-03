05589aadc359f8e7924cd9eb958ec8c3044072ab
ThePrimeagen <markcromwell@gmail.com>
2025-11-02 22:21:12 -0800

    save

--- src/main.py
+++ src/main.py
@@ -5,7 +5,15 @@ from copy_static import copy_static_files
 
 
 def main():
+    """
+    Delete anything in the public directory.
+    Copy all the static files from static to public.
+    Generate a page from content/index.md using template.html and write it to public/index.html.
+    """
+    from generage_page import generate_page
     copy_static_files("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
+    
 
 if __name__ == "__main__":
     main()
